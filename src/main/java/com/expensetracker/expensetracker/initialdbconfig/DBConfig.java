e4a7a6df8dcec3c779b2b015524bee6c4344b631
Jonas Hauth <jdhauth@web.de>
2021-06-27 14:54:54 +0200

    Added Expenses to Initial DB Config

--- src/main/java/com/expensetracker/expensetracker/initialdbconfig/DBConfig.java
+++ src/main/java/com/expensetracker/expensetracker/initialdbconfig/DBConfig.java
@@ -17,23 +17,23 @@ public class DBConfig {
 
     @ChangeSet(order= "001", id = "seedBudget", author = "InitialConfig")
     public void seedBudget(BudgetRepository budgetRepository){
-        Budget ausbildungUndStudium = new Budget("Ausbildung und Studium", 2000d, "1");
+        Budget ausbildungUndStudium = new Budget("Ausbildung und Studium", 100d, "1");
         budgetRepository.save(ausbildungUndStudium);
-        Budget reisen = new Budget("Reisen", 2000d, "1");
+        Budget reisen = new Budget("Reisen", 300d, "1");
         budgetRepository.save(reisen);
-        Budget infrastruktur = new Budget("Infrastruktur", 2000d, "1");
+        Budget infrastruktur = new Budget("Infrastruktur", 200d, "1");
         budgetRepository.save(infrastruktur);
-        Budget mobilität = new Budget("Mobilität", 2000d, "1");
+        Budget mobilität = new Budget("Mobilität", 200d, "1");
         budgetRepository.save(mobilität);
-        Budget bekleidung = new Budget("Bekleidung", 2000d, "1");
+        Budget bekleidung = new Budget("Bekleidung", 100d, "1");
         budgetRepository.save(bekleidung);
-        Budget freizeit = new Budget("Freizeit", 2000d, "1");
+        Budget freizeit = new Budget("Freizeit", 100d, "1");
         budgetRepository.save(freizeit);
-        Budget versicherung = new Budget("Versicherung", 2000d, "1");
+        Budget versicherung = new Budget("Versicherung", 200d, "1");
         budgetRepository.save(versicherung);
-        Budget sonstigeAusgaben = new Budget("Sonstige Ausgaben", 2000d, "1");
+        Budget sonstigeAusgaben = new Budget("Sonstige Ausgaben", 100d, "1");
         budgetRepository.save(sonstigeAusgaben);
-        Budget essenUndTrinken = new Budget("Essen und Trinken", 2000d, "1");
+        Budget essenUndTrinken = new Budget("Essen und Trinken", 300d, "1");
         budgetRepository.save(essenUndTrinken);
     }
 
@@ -41,6 +41,48 @@ public class DBConfig {
     public void seedUser(UserRepository userRepository) {
         User standaloneUser = new User("1", "StandaloneUser", "****", "test@email.de", LocalDateTime.now().minusMonths(6));
         userRepository.save(standaloneUser);
+    }
+
+    @ChangeSet(order= "003", id = "seedExpenses", author = "InitialConfig")
+    public void seedExpenses(ExpenseRepository expenseRepository) {
+        // Ausbildung und Studium
+        Expense expense1 = new Expense(LocalDateTime.now().minusMonths(6), "Semestergebühren", 60d, "Ausbildung und Studium", "1");
+        expenseRepository.save(expense1);
+        Expense expense2 = new Expense(LocalDateTime.now().minusMonths(5), "Semestergebühren", 60d, "Ausbildung und Studium", "1");
+        expenseRepository.save(expense2);
+        Expense expense3 = new Expense(LocalDateTime.now().minusMonths(4), "Semestergebühren", 60d, "Ausbildung und Studium", "1");
+        expenseRepository.save(expense3);
+        Expense expense4 = new Expense(LocalDateTime.now().minusMonths(3), "Semestergebühren", 60d, "Ausbildung und Studium", "1");
+        expenseRepository.save(expense4);
+        Expense expense5 = new Expense(LocalDateTime.now().minusMonths(2), "Semestergebühren", 60d, "Ausbildung und Studium", "1");
+        expenseRepository.save(expense5);
+        Expense expense6 = new Expense(LocalDateTime.now().minusMonths(2), "Bücher", 70d, "Ausbildung und Studium", "1");
+        expenseRepository.save(expense6);
+        Expense expense15 = new Expense(LocalDateTime.now().minusMonths(1), "Semestergebühren", 60d, "Ausbildung und Studium", "1");
+        expenseRepository.save(expense15);
+
+        // Freizeit
+        Expense expense7 = new Expense(LocalDateTime.now().minusMonths(4), "Europapark", 120d, "Freizeit", "1");
+        expenseRepository.save(expense7);
+        Expense expense8 = new Expense(LocalDateTime.now().minusMonths(3), "Ago", 140d, "Freizeit", "1");
+        expenseRepository.save(expense8);
+
+        // Bekleidung
+        Expense expense9 = new Expense(LocalDateTime.now().minusMonths(3), "Anzug", 200d, "Bekleidung", "1");
+        expenseRepository.save(expense9);
+        Expense expense10 = new Expense(LocalDateTime.now().minusMonths(5), "Schuhe", 80d, "Bekleidung", "1");
+        expenseRepository.save(expense10);
+        Expense expense11 = new Expense(LocalDateTime.now().minusMonths(0), "Schuhe", 80d, "Bekleidung", "1");
+        expenseRepository.save(expense11);
+
+        // Infrastruktur
+        Expense expense12 = new Expense(LocalDateTime.now().minusMonths(2), "Handyvertrag", 15d, "Infrastruktur", "1");
+        expenseRepository.save(expense12);
+        Expense expense13 = new Expense(LocalDateTime.now().minusMonths(1), "Handyvertrag", 15d, "Infrastruktur", "1");
+        expenseRepository.save(expense13);
+        Expense expense14 = new Expense(LocalDateTime.now().minusMonths(0), "Handyvertrag", 15d, "Infrastruktur", "1");
+        expenseRepository.save(expense14);
+
     }
 
 }
